0dc1a7c525c23a58f46adb4647bfd1c207ce0014
francisco2espinoza <francisco2espinoza@icloud.com>
2024-04-14 00:08:50 -0500

    add_homeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { AboutusComponent } from './public/aboutus/aboutus.component';
 import { ContactusComponent } from './public/contactus/contactus.component';
+import { TeamComponent } from './public/team/team.component';
 
 export const routes: Routes = [
     {path: "home", component: HomeComponent},
     {path: "aboutus", component: AboutusComponent},
-    {path: "contactus", component: ContactusComponent}
+    {path: "contactus", component: ContactusComponent},
+    {path: "team", component: TeamComponent}
 ];
