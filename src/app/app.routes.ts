a179f25c390c51e029bca5f525e1b921a41b15cd
francisco2espinoza <francisco2espinoza@icloud.com>
2024-04-13 23:43:35 -0500

    enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './public/home/home.component';
+import { AboutusComponent } from './public/aboutus/aboutus.component';
+import { ContactusComponent } from './public/contactus/contactus.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: HomeComponent},
+    {path: "aboutus", component: AboutusComponent},
+    {path: "contactus", component: ContactusComponent}
+];
